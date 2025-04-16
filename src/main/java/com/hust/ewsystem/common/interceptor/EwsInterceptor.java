f3371abea62cbf3b0f16bdc1a7dbadd5ba77e5c7
dmjkk <1966423145@qq.com>
2025-04-16 11:01:49 +0800

    项目初始化

--- src/main/java/com/hust/ewsystem/common/interceptor/EwsInterceptor.java
+++ src/main/java/com/hust/ewsystem/common/interceptor/EwsInterceptor.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class EwsInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String token = request.getHeader("Authorization");
         if (!isValidToken(token)) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
